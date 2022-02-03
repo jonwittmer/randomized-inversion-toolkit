fixed noise covariance error

examples/regularization_toolbox_examples/deriv2.py
@@ -11,7 +11,8 @@ from src.randomization_strategies import Strategies
 from src.random_sampling import scaledIdentityCovGenerator
 from utils.generate_figures import generateFigures
 from utils.generate_tables import writeLatexTables
-    
+from utils.lcurve import computeLCurve
+
 def trueParameter(observation_coords):
     n_observations = observation_coords.shape[0]
     dx = 1 / n_observations 
@@ -44,7 +45,7 @@ if __name__ == '__main__':
     # problem setup
     n_observations = 1000
     noise_level = 0.01
-    regularization = 6.5 # we will use identiy prior_covariance, parameterized by scalar given here
+    regularization = 750 # we will use identiy prior_covariance, parameterized by scalar given here
     #random_vector_generator = np.random.multivariate_normal
     random_vector_generator = scaledIdentityCovGenerator
     
@@ -57,33 +58,22 @@ if __name__ == '__main__':
     solver_type = 'direct'
     problem_name = 'Deriv2'
 
+    def strategyBuilder(reg):
+        return Strategies.NO_RANDOMIZATION(data, forward_map, 1 / (noise_std**2), 0, reg, random_vector_generator, 0, solver_type)
+    computeLCurve(true_parameter, strategyBuilder)
+
     # generate u1 solution only once
     no_randomizaton_solver = Strategies.NO_RANDOMIZATION(data, forward_map, 1 / (noise_std**2), 0, regularization, random_vector_generator, 0, solver_type)
     u1_solution = no_randomizaton_solver.solve()
-
-    # l-curve
-    regs  = np.logspace(-5, 5, 15)
-    lcurve_sols = []
-    for reg in regs:
-        no_randomization_solver = Strategies.NO_RANDOMIZATION(data, forward_map, 1 / (noise_std**2), 0, reg, random_vector_generator, 0, solver_type)
-        no_randomization_solver.solver.atol = 1e-5
-        no_randomization_solver.solver.tol = 1e-5
-        no_randomization_solver.solver.maxiter = 1000
-        u1_solution = no_randomization_solver.solve().reshape(true_parameter.shape)
-        lcurve_sols.append(np.linalg.norm(u1_solution - true_parameter) / np.linalg.norm(true_parameter))
-
-    fig, ax = plt.subplots()
-    ax.plot(regs, lcurve_sols)
-    plt.show()
     
     n_random_samples = [10, 100, 1000, 10000]
     test_strategies = [
         Strategies.RMAP,
         Strategies.RMA,
-        # Strategies.RMA_RMAP,
-        # Strategies.RS_U1,
-        # Strategies.RS,
-        # Strategies.ENKF,
+        Strategies.RMA_RMAP,
+        Strategies.RS_U1,
+        Strategies.RS,
+        Strategies.ENKF,
     ]
     results = {}
     
@@ -102,6 +92,6 @@ if __name__ == '__main__':
             print(f"N = {samples}    error = {results[randomized_solver.name]['rel_error'][-1]}")
         print()
         generateFigures(observation_coords, u1_solution, rand_solutions, rand_labels,
-                        f"figures/{problem_name}/{randomized_solver.name}.png", lims={"ylim": [-.01, .01]})
+                        f"figures/{problem_name}/{randomized_solver.name}.pdf", lims={"ylim": [-.01, .03]})
 
     writeLatexTables(results, f'{problem_name}_table.tex')

examples/regularization_toolbox_examples/shaw.py
@@ -8,6 +8,7 @@ import scipy.ndimage
 import matplotlib.pyplot as plt
 
 from src.randomization_strategies import Strategies
+from src.random_sampling import scaledIdentityCovGenerator
 from utils.generate_figures import generateFigures
 from utils.generate_tables import writeLatexTables
 
@@ -55,18 +56,19 @@ if __name__ == '__main__':
     n_observations = 1000
     noise_level = 0.01
     regularization = 200 # we will use identiy prior_covariance, parameterized by scalar given here
-    random_vector_generator = np.random.multivariate_normal
+    random_vector_generator = scaledIdentityCovGenerator
     
     observation_coords, observations = generateObservations(n_observations)
     true_parameter = trueParameter(observation_coords)
     data = observations + np.amax(observations) * np.random.normal(0, noise_level, observations.shape)
+    noise_std = noise_level * np.amax(observations)
     forward_map = buildForwardMatrix(n_observations)
     
     solver_type = 'direct'
     problem_name = 'Shaw'
 
     # generate u1 solution only once
-    no_randomizaton_solver = Strategies.NO_RANDOMIZATION(data, forward_map, 1 / (noise_level**2), 0, regularization, random_vector_generator, 0, solver_type)
+    no_randomizaton_solver = Strategies.NO_RANDOMIZATION(data, forward_map, 1 / (noise_std**2), 0, regularization, random_vector_generator, 0, solver_type)
     u1_solution = no_randomizaton_solver.solve()
     
     n_random_samples = [10, 100, 1000, 10000]
@@ -85,7 +87,7 @@ if __name__ == '__main__':
         rand_labels = []
         for samples in n_random_samples:
             rand_labels.append(f"N = {samples}")
-            randomized_solver = curr_strategy(data, forward_map, 1 / (noise_level**2), 0, regularization, random_vector_generator, samples, solver_type)
+            randomized_solver = curr_strategy(data, forward_map, 1 / (noise_std**2), 0, regularization, random_vector_generator, samples, solver_type)
             rand_solutions.append(randomized_solver.solve())
             
             if randomized_solver.name not in results:
@@ -95,6 +97,6 @@ if __name__ == '__main__':
             print(f"N = {samples}    error = {results[randomized_solver.name]['rel_error'][-1]}")
         print()
         generateFigures(observation_coords, u1_solution, rand_solutions, rand_labels,
-                        f"figures/{problem_name}/{randomized_solver.name}.png", lims={"ylim": [-2, 2]})
+                        f"figures/{problem_name}/{randomized_solver.name}.pdf", lims={"ylim": [-2, 2]})
 
     writeLatexTables(results, f'{problem_name}_table.tex')

examples/xray_tomography/xray_tomography.py
@@ -11,8 +11,10 @@ import skimage.transform as sk_transform
 import matplotlib.pyplot as plt
 
 from src.randomization_strategies import Strategies
+from src.random_sampling import scaledIdentityCovGenerator
 from utils.generate_tables import writeLatexTables
 from utils.generate_figures import checkDirectory
+from utils.lcurve import computeLCurve
 
 def trueParameter(dimension):
     original = sk_data.shepp_logan_phantom()
@@ -49,7 +51,7 @@ def saveSolution(solution, problem_name, method_name, n_samples):
     fig, ax = plt.subplots(figsize=(3,3))
     ax.imshow(solution, vmin=0, vmax=1, cmap='gray')
     ax.axis('off')
-    fig_name = f'figures/{problem_name}/{method_name}_N{n_samples:04d}.png'
+    fig_name = f'figures/{problem_name}/{method_name}_N{n_samples:04d}.pdf'
     checkDirectory(fig_name)
     plt.savefig(fig_name, pad_inches=0, dpi=300)
     plt.close(fig)
@@ -62,53 +64,38 @@ if __name__ == '__main__':
     image_dimension = 128
     noise_level = 0.01
     regularization = 2500 # we will use identiy prior_covariance, parameterized by scalar given here
-
-    # faster random sample generator for scaled identity covariance
-    def scaledIdentityCovGenerator(mean, cov, n_random_vectors):
-        vecs = np.random.normal(0, cov[0, 0], (mean.shape[0], n_random_vectors)) + mean[:, np.newaxis]
-        return vecs.T
     random_vector_generator = scaledIdentityCovGenerator
     
     true_parameter = trueParameter(image_dimension)
     observations = generateObservations(true_parameter, n_angles)
     forward_map = buildForwardOperator(true_parameter.shape, observations.shape)
     data = observations + np.amax(observations) * np.random.normal(0, noise_level, observations.shape)
+    noise_std = noise_level * np.amax(observations)
     data = data.reshape((-1,))
     
     solver_type = 'cg'
     problem_name = 'xray_tomography'
 
+    def strategyBuilder(reg):
+        return Strategies.NO_RANDOMIZATION(data, forward_map, 1 / (noise_std**2), 0, reg, random_vector_generator, 0, solver_type)
+    computeLCurve(true_parameter, strategyBuilder)
+    
     # generate u1 solution only once
-    no_randomization_solver = Strategies.NO_RANDOMIZATION(data, forward_map, 1 / (noise_level**2), 0, regularization, random_vector_generator, 0, solver_type)
+    no_randomization_solver = Strategies.NO_RANDOMIZATION(data, forward_map, 1 / (noise_std**2), 0, regularization, random_vector_generator, 0, solver_type)
     no_randomization_solver.solver.atol = 1e-5
     no_randomization_solver.solver.tol = 1e-5
     no_randomization_solver.solver.maxiter = 2000
     u1_solution = no_randomization_solver.solve().reshape(true_parameter.shape)
     saveSolution(u1_solution, problem_name, 'u1', 0)
-
-    # l-curve
-    # regs  = np.logspace(-5, 5, 15)
-    # lcurve_sols = []
-    # for reg in regs:
-    #     no_randomization_solver = Strategies.NO_RANDOMIZATION(data, forward_map, 1 / (noise_level**2), 0, reg, random_vector_generator, 0, solver_type)
-    #     no_randomization_solver.solver.atol = 1e-5
-    #     no_randomization_solver.solver.tol = 1e-5
-    #     no_randomization_solver.solver.maxiter = 1000
-    #     u1_solution = no_randomization_solver.solve().reshape(true_parameter.shape)
-    #     lcurve_sols.append(np.linalg.norm(u1_solution - true_parameter) / np.linalg.norm(true_parameter))
-
-    # fig, ax = plt.subplots()
-    # ax.plot(regs, lcurve_sols)
-    # plt.show()
     
     n_random_samples = [10, 1000, 5000]
     test_strategies = [
-        #Strategies.RMAP,
+        Strategies.RMAP,
         Strategies.RMA,
-        #Strategies.RMA_RMAP,
+        Strategies.RMA_RMAP,
         Strategies.RS_U1,
         Strategies.RS,
-        #Strategies.ENKF,
+        Strategies.ENKF,
     ]
     results = {}
     
@@ -117,7 +104,7 @@ if __name__ == '__main__':
         rand_labels = []
         for samples in n_random_samples:
             rand_labels.append(f"N = {samples}")
-            randomized_solver = curr_strategy(data, forward_map, 1 / (noise_level**2), 0, regularization, random_vector_generator, samples, solver_type)
+            randomized_solver = curr_strategy(data, forward_map, 1 / (noise_std**2), 0, regularization, random_vector_generator, samples, solver_type)
             randomized_solver.solver.atol = 1e-5
             randomized_solver.solver.tol = 1e-5
             randomized_solver.solver.maxiter = 2000
@@ -131,6 +118,5 @@ if __name__ == '__main__':
 
             saveSolution(rand_solutions[-1], problem_name, randomized_solver.name, samples)
         print()
-
         
     writeLatexTables(results, f'{problem_name}_table.tex')

src/randomization_strategies.py
@@ -230,14 +230,14 @@ class LeftSketchingRmapStrategy(RandomizationStrategy):
         self.projection_vectors = self.random_vector_generator(np.zeros_like(self.data), self.inv_noise_covariance, self.n_random_samples).T
         self.projection_vectors *= 1 / (self.n_random_samples**0.5)
         
-    def solveRealization(self, data_perturbation, prior_perturbation):
+    def solveRealization(self, perturbed_data, perturbed_prior_mean):
         # form hessian if not already formed
         if self.hessian is None:
             self.formHessian()
             
         # form right hand side without storing intermediate matrices (only matvec products)
-        rhs = self.projection_vectors @ (self.projection_vectors.T @ (self.data + data_perturbation))
-        rhs = self.forward_map.transpose() @ rhs + self.inv_prior_covariance @ (self.prior_mean + prior_perturbation)
+        rhs = self.projection_vectors @ (self.projection_vectors.T @ perturbed_data)
+        rhs = self.forward_map.transpose() @ rhs + self.inv_prior_covariance @ perturbed_prior_mean
             
         return self.solver.solve(self.hessian, rhs)
 
@@ -250,12 +250,22 @@ class LeftSketchingRmapStrategy(RandomizationStrategy):
         self.convertCovariancesToMatrices()
         self.drawRandomVectors()
         
-        # allocate storage for all realizations
-        results = np.zeros((self.parameter_dim, self.n_random_samples))
-        for i in range(self.n_random_samples):
-            data_perturbation = self.random_vector_generator(np.zeros_like(self.data), self.noise_covariance, 1).reshape(self.data.shape)
-            prior_perturbation = self.random_vector_generator(np.zeros_like(self.prior_mean), self.prior_covariance, 1).reshape(self.prior_mean.shape)
-            results[:, i] = np.reshape(self.solveRealization(data_perturbation, prior_perturbation), (self.parameter_dim,))
+        if self.solver.solver_type == 'direct':
+            perturbed_data = np.tile(self.data.reshape((self.data.shape[0], 1)), (1, self.n_random_samples))
+            perturbed_prior_mean = np.tile(self.prior_mean.reshape((self.prior_mean.shape[0], 1)), (1, self.n_random_samples))
+            print('sampling')
+            for i in range(self.n_random_samples):
+                perturbed_data[:, i] += self.random_vector_generator(np.zeros_like(self.data), self.noise_covariance, 1).reshape(self.data.shape)
+                perturbed_prior_mean[:, i] += self.random_vector_generator(np.zeros_like(self.prior_mean), self.prior_covariance, 1).reshape(self.prior_mean.shape)
+            print('solving system')
+            results = self.solveRealization(perturbed_data, perturbed_prior_mean)
+        else:
+            # allocate storage for all realizations
+            results = np.zeros((self.parameter_dim, self.n_random_samples))
+            for i in range(self.n_random_samples):
+                data_perturbation = self.random_vector_generator(np.zeros_like(self.data), self.noise_covariance, 1).reshape(self.data.shape)
+                prior_perturbation = self.random_vector_generator(np.zeros_like(self.prior_mean), self.prior_covariance, 1).reshape(self.prior_mean.shape)
+                results[:, i] = np.reshape(self.solveRealization(self.data + data_perturbation, self.prior_mean + prior_perturbation), (self.parameter_dim,))
         return np.mean(results, axis=1)
     
     
@@ -359,14 +369,14 @@ class EnkfStrategy(RandomizationStrategy):
         else:
             self.innovation = self.forward_map @ self.projection_vectors @ self.projection_vectors.T @ self.forward_map.T + self.noise_covariance
         
-    def solveRealization(self, data_perturbation, prior_perturbation):
+    def solveRealization(self, perturbed_data, perturbed_prior_mean):
         # innovation name comes from Kalman filtering literature
         if self.innovation is None:
             self.formInnovation()
             
-        rhs = self.data + data_perturbation - self.forward_map @ (self.prior_mean + prior_perturbation)
+        rhs = perturbed_data - self.forward_map @ perturbed_prior_mean
         temp = self.solver.solve(self.innovation, rhs)
-        return self.prior_mean +  self.projection_vectors @ (self.projection_vectors.T @ (self.forward_map.transpose() @ temp))
+        return perturbed_prior_mean +  self.projection_vectors @ (self.projection_vectors.T @ (self.forward_map.transpose() @ temp))
 
     # allocate storage for all realizations
     def solve(self):
@@ -376,12 +386,22 @@ class EnkfStrategy(RandomizationStrategy):
             self.prior_covariance = self.getCovarianceFromInverse(self.inv_prior_covariance)
         self.convertCovariancesToMatrices()
         self.drawRandomVectors()
-        
-        results = np.zeros((self.parameter_dim, self.n_random_samples))
-        for i in range(self.n_random_samples):
-            data_perturbation = self.random_vector_generator(np.zeros_like(self.data), self.noise_covariance, 1).reshape(self.data.shape)
-            prior_perturbation = self.random_vector_generator(np.zeros_like(self.prior_mean), self.prior_covariance, 1).reshape(self.prior_mean.shape)
-            results[:, i] = np.reshape(self.solveRealization(data_perturbation, prior_perturbation), (self.parameter_dim,))
+
+        if self.solver.solver_type == 'direct':
+            perturbed_data = np.tile(self.data.reshape((self.data.shape[0], 1)), (1, self.n_random_samples))
+            perturbed_prior_mean = np.tile(self.prior_mean.reshape((self.prior_mean.shape[0], 1)), (1, self.n_random_samples))
+            print('sampling')
+            for i in range(self.n_random_samples):
+                perturbed_data[:, i] += self.random_vector_generator(np.zeros_like(self.data), self.noise_covariance, 1).reshape(self.data.shape)
+                perturbed_prior_mean[:, i] += self.random_vector_generator(np.zeros_like(self.prior_mean), self.prior_covariance, 1).reshape(self.prior_mean.shape)
+            print('solving system')
+            results = self.solveRealization(perturbed_data, perturbed_prior_mean)
+        else:
+            results = np.zeros((self.parameter_dim, self.n_random_samples))
+            for i in range(self.n_random_samples):
+                data_perturbation = self.random_vector_generator(np.zeros_like(self.data), self.noise_covariance, 1).reshape(self.data.shape)
+                prior_perturbation = self.random_vector_generator(np.zeros_like(self.prior_mean), self.prior_covariance, 1).reshape(self.prior_mean.shape)
+                results[:, i] = np.reshape(self.solveRealization(self.data + data_perturbation, self.prior_mean + prior_perturbation), (self.parameter_dim,))
         return np.mean(results, axis=1)
 
 class Strategies: